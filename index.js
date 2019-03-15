2929b958b0c3adb5fef494c26e2191eb1fa4c6c7
loocash <lukaszkapica@gmail.com>
2019-03-15 15:08:11 +0100

    Add jsdoc

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Set up default values
 const TILE_SIDE  = 50;
 const FREE_SPACE = 8;
 const OFFSET = FREE_SPACE+TILE_SIDE;
@@ -7,6 +8,7 @@ const COLS = 4;
 let hiscore = 1000000;
 let moves_counter = 0;
 
+// Load elements
 const Elements = _.mapValues({
   board:        '#board',
   moves:        '#moves',
@@ -16,6 +18,11 @@ const Elements = _.mapValues({
   shuffle:      '#shuffle'
 }, queryString => document.querySelector(queryString));
 
+/**
+ * Makes element blink on the screen
+ * @param {HTMLElement} element - target element
+ * @param {number} times - number of blinks
+ */
 const blink = (element, times = 6) => {
   if (times <= 0) return;
   const visible = times % 2 === 1;
@@ -23,20 +30,36 @@ const blink = (element, times = 6) => {
   return setTimeout(() => blink(element, times-1), 250);
 };
 
+// bottom-right is always a free space at the beginning
 const freeTile = { row: ROWS-1, col: COLS-1 };
 
-const adjacent = (tile1, tile2) => 
+/**
+ * Returns true if two tiles are next to each other in one of the four directions (N, S, W, E)
+ * @param {Tile} tile1 - first tile
+ * @param {Tile} tile2 - second tile
+ */
+const nextTo = (tile1, tile2) =>
   Math.abs(tile1.row-tile2.row) + 
   Math.abs(tile1.col-tile2.col) === 1;
 
+/**
+ * Returns true if all tiles are in their initial positions
+ */
 const solved = () => Elements.tiles.every(tile => 
   tile.row == tile.dataset.row && 
   tile.col == tile.dataset.col);
 
+/**
+ * Returns true if tile's row and col coordinates are within board's boundaries
+ * @param {Tile} tile
+ */
 const inBounds = ({row, col}) => 
   _.inRange(row, ROWS) && 
   _.inRange(col, COLS);
 
+/**
+ * Returns a random tile which can be moved towards the free space
+ */
 const randomTile = () => {
   const {row, col} = freeTile;
   return _.sample([
@@ -47,8 +70,20 @@ const randomTile = () => {
   ].filter(inBounds));
 };
 
-const getTile = ({row, col}) => Elements.tiles.find(tile => tile.row == row && tile.col == col);
+/**
+ * Returns an HTMLElement corresponding to the tile
+ * @param {tile} tile
+ */
+const getTile = ({row, col}) =>
+  Elements.tiles.find(
+    tile => tile.row == row &&
+            tile.col == col);
 
+/**
+ * Moves a tile relative to the free space
+ * @param {number} drow - diffrence in rows
+ * @param {number} dcol - difference in cols
+ */
 const moveFrom = (drow, dcol) => {
   const t = {row: freeTile.row+drow, col: freeTile.col+dcol};
   if (inBounds(t)) {
@@ -56,8 +91,16 @@ const moveFrom = (drow, dcol) => {
   }
 };
 
+/**
+ * Returns a pixel position of the upper-left corner of the tile
+ * @param {Tile} tile
+ */
 const computeXY = ({row, col}) => ({ dx: col*OFFSET, dy: row*OFFSET });
 
+/**
+ * Swaps two tiles on the board
+ * @param {Tile} tile
+ */
 const swapWith = (tile) => {
   const {dx, dy} = computeXY({
     row: freeTile.row - tile.dataset.row,
@@ -71,8 +114,12 @@ const swapWith = (tile) => {
   tile.style.transform = `translate(${dx}px, ${dy}px)`;
 };
 
+/**
+ * Moves a tile towards the free space
+ * @param {Tile} tile
+ */
 const moveTile = (tile) => {
-  if (!adjacent(freeTile, tile) || solved()) return;
+  if (!nextTo(freeTile, tile) || solved()) return;
   moves_counter += 1;
   Elements.moves.innerText = moves_counter;
   swapWith(tile);
@@ -86,11 +133,17 @@ const moveTile = (tile) => {
   }
 };
 
+/**
+ * Shuffles the board
+ */
 const shuffle = () => {
   _.times(ROWS*ROWS*COLS*COLS, () => swapWith(getTile(randomTile())));
   moves_counter = 0;    
   Elements.moves.innerText = moves_counter;
 };
+
+
+// Set up event listeners
 
 const keyLocks = [];
 
@@ -120,6 +173,7 @@ document.addEventListener('keydown', event => {
 
 document.addEventListener('keyup', ({keyCode}) => delete keyLocks[keyCode]);
 
+// populate the board with tiles
 Elements.tiles = 
   _.range(ROWS*COLS - 1).map(i => {
     const row = Math.floor(i / ROWS);
